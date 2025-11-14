53bb54b09c02421872e71af23e1c5c44f38fc911
zarakikenpashi <sadiarog.abel@gmail.com>
2025-11-14 10:23:26 +0100

    push on netifly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -50,7 +50,7 @@ export const Footer = () => {
               <div class="md:col-span-2">
                   <a aria-label="go home" class="block size-fit" href="/"
                       ><svg
-                          class="text-foreground h-6 w-full"
+                          class="text-white h-6 w-full"
                           viewBox="0 0 797 220"
                           fill="none"
                           xmlns="http://www.w3.org/2000/svg"
@@ -70,8 +70,8 @@ export const Footer = () => {
                                   <stop stop-color="#9B99FE"></stop>
                                   <stop offset="1" stop-color="#2BC8B7"></stop>
                               </linearGradient>
-                          </defs></svg
-                  ></a>
+                          </defs></svg>
+                          </a>
 
                   <div class="flex flex-wrap  gap-6 text-sm py-6">
                       <a
@@ -84,8 +84,8 @@ export const Footer = () => {
                               <path
                                   fill="currentColor"
                                   d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
-                              ></path></svg></a
-                      ><a
+                              ></path></svg></a>
+                              <a
                           target="_blank"
                           rel="noopener noreferrer"
                           aria-label="Facebook"
@@ -95,8 +95,7 @@ export const Footer = () => {
                               <path
                                   fill="currentColor"
                                   d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
-                              ></path></svg></a
-                      >
+                              ></path></svg></a>
                       <a
                           target="_blank"
                           rel="noopener noreferrer"
@@ -107,8 +106,7 @@ export const Footer = () => {
                               <path
                                   fill="currentColor"
                                   d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
-                              ></path></svg></a
-                      ><a
+                              ></path></svg></a><a
                           target="_blank"
                           rel="noopener noreferrer"
                           aria-label="TikTok"
@@ -118,25 +116,24 @@ export const Footer = () => {
                               <path
                                   fill="currentColor"
                                   d="M16.6 5.82s.51.5 0 0A4.28 4.28 0 0 1 15.54 3h-3.09v12.4a2.59 2.59 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48"
-                              ></path></svg
-                      ></a>
+                              ></path></svg></a>
                   </div>
               </div>
               <div class="md:col-span-2 md:grid-cols-2 grid  gap-6">
                   <div class="space-y-4 text-sm">
-                      <span class="block font-medium">Liens utiles</span>
+                      <span class="block font-medium text-muted-foreground">Liens utiles</span>
                       <div className="grid gap-y-3 gap-x-4 grid-cols-4  md:flex md:flex-wrap md:w-full">
-                      <a class="text-muted-foreground hover:text-primary block duration-150"><span>Features</span></a>
-                      <a class="text-muted-foreground hover:text-primary block duration-150"><span>Solution</span></a>
-                      <a class="text-muted-foreground hover:text-primary block duration-150"><span>Customers</span></a>
-                      <a class="text-muted-foreground hover:text-primary block duration-150"><span>Pricing</span></a>
-                      <a class="text-muted-foreground hover:text-primary block duration-150"><span>Help</span></a>
-                      <a class="text-muted-foreground hover:text-primary block duration-150"><span>About</span></a>
+                      <a class="text-white hover:text-primary block duration-150"><span>Features</span></a>
+                      <a class="text-white hover:text-primary block duration-150"><span>Solution</span></a>
+                      <a class="text-white hover:text-primary block duration-150"><span>Customers</span></a>
+                      <a class="text-white hover:text-primary block duration-150"><span>Pricing</span></a>
+                      <a class="text-white hover:text-primary block duration-150"><span>Help</span></a>
+                      <a class="text-white hover:text-primary block duration-150"><span>About</span></a>
                       </div>
 
                   </div>
                   <div class="space-y-4 text-sm">
-                      <span class="block font-medium">Payment Methods</span>
+                      <span class="block font-medium text-muted-foreground">Payment Methods</span>
                       <div className="payment-body grid gap-y-3 gap-x-4 grid-cols-5 md:flex md:flex-wrap md:w-full">
                         <img
                           class="bg-white rounded-sx inline-block h-7 w-10"
@@ -196,7 +193,7 @@ export const Footer = () => {
               </div>
           </div>
           <div class="mt-12 flex flex-wrap items-end justify-between gap-6 border-t py-6">
-    <small class="text-muted-foreground order-last block text-center text-sm md:order-first">
+    <small class="text-white order-last block text-center text-sm md:order-first">
         ©2025 kimiia, All rights reserved
     </small>
     <form action="">
